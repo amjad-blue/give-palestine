7ab29c69d3aae114eaef521be00cb87950edaed1
amjad-blue <amjad@blue.me>
2025-08-25 15:43:35 +0300

    add new changes

--- js/app.js
+++ js/app.js
@@ -225,19 +225,18 @@ function handleHomePage() {
 		// partners-swiper
 		new Swiper(".partners-swiper", {
 			slidesPerView: "auto",
-			spaceBetween: 30,
+			spaceBetween: 20,
+			loop: true,
+			loopedSlides: 6,
 			breakpoints: {
 				640: { slidesPerView: 3 },
-				1024: { slidesPerView: 6 },
+				1024: { slidesPerView: 5 },
 			},
-			pagination: {
-				el: ".swiper-pagination",
-				clickable: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
 			},
-			autoplay: {
-				delay: 5000,
-				disableOnInteraction: true,
-			},
+
 			keyboard: {
 				enabled: true,
 			},
